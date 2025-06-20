952da74536b6c9a0dba3ca3ee5161974bb7033ea
Claudio Lopez <claudiols1979@hotmail.com>
2025-06-19 21:37:25 -0600

    update

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
-const API_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:5000";
+const isProduction = process.env.NODE_ENV === "production";
+
+const API_URL = isProduction ? process.env.REACT_APP_BACKEND_URL : "http://localhost:5000";
 
 export default API_URL;
